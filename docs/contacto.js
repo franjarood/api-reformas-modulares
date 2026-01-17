33b2c3fb446d60fb60569daedf3cf284e0f85963
franjarood <franjarood@gmail.com>
2026-01-17 17:31:54 +0100

    cambios js

--- docs/contacto.js
+++ docs/contacto.js
@@ -1,5 +1,5 @@
-console.log("API_BASE_URL =", API_BASE_URL);
 const API_BASE_URL = window.API_BASE_URL || "http://127.0.0.1:8000";
+console.log("API_BASE_URL =", API_BASE_URL);
 
 
 document.addEventListener("DOMContentLoaded", () => {

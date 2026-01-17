3629e14b8aa91d2e9e0718b320d75910b4469fe4
franjarood <franjarood@gmail.com>
2026-01-17 13:42:50 +0100

    Add config.js

--- docs/contacto.js
+++ docs/contacto.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = window.API_BASE_URL || "http://127.0.0.1:8000";
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".form-contacto");
